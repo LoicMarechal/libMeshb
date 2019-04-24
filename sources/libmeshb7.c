d33e8c96c2e1ccb7da496b9c50546db7be557ea1
Loïc Maréchal <loic.marechal@inria.fr>
2019-04-24 16:27:30 +0200

    Added P1 elements to the HO renumbering procedure.

--- sources/libmeshb7.c
+++ sources/libmeshb7.c
@@ -2,14 +2,14 @@
 
 /*----------------------------------------------------------------------------*/
 /*                                                                            */
-/*                               LIBMESH V 7.50                               */
+/*                               LIBMESH V 7.51                               */
 /*                                                                            */
 /*----------------------------------------------------------------------------*/
 /*                                                                            */
 /*   Description:        handles .meshb file format I/O                       */
 /*   Author:             Loic MARECHAL                                        */
 /*   Creation date:      dec 09 1999                                          */
-/*   Last modification:  mar 11 2019                                          */
+/*   Last modification:  apr 14 2019                                          */
 /*                                                                            */
 /*----------------------------------------------------------------------------*/
 
@@ -2420,24 +2420,31 @@ int GmfSetHONodesOrdering(int64_t MshIdx, int KwdCod, int *BasTab, int *OrdTab)
    // Find the Bezier indices dimension according to the element's kind
    switch(KwdCod)
    {
+      case GmfEdges   :          NmbNod =  2; NmbCrd = 1; break;
       case GmfEdgesP2 :          NmbNod =  3; NmbCrd = 1; break;
       case GmfEdgesP3 :          NmbNod =  4; NmbCrd = 1; break;
       case GmfEdgesP4 :          NmbNod =  5; NmbCrd = 1; break;
+      case GmfTriangles   :      NmbNod =  3; NmbCrd = 3; break;
       case GmfTrianglesP2 :      NmbNod =  6; NmbCrd = 3; break;
       case GmfTrianglesP3 :      NmbNod = 10; NmbCrd = 3; break;
       case GmfTrianglesP4 :      NmbNod = 15; NmbCrd = 3; break;
+      case GmfQuadrilaterals   : NmbNod =  4; NmbCrd = 2; break;
       case GmfQuadrilateralsQ2 : NmbNod =  9; NmbCrd = 2; break;
       case GmfQuadrilateralsQ3 : NmbNod = 16; NmbCrd = 2; break;
       case GmfQuadrilateralsQ4 : NmbNod = 25; NmbCrd = 2; break;
+      case GmfTetrahedra   :     NmbNod =  4; NmbCrd = 4; break;
       case GmfTetrahedraP2 :     NmbNod = 10; NmbCrd = 4; break;
       case GmfTetrahedraP3 :     NmbNod = 20; NmbCrd = 4; break;
       case GmfTetrahedraP4 :     NmbNod = 35; NmbCrd = 4; break;
+      case GmfPyramids   :       NmbNod =  5; NmbCrd = 3; break;
       case GmfPyramidsP2 :       NmbNod = 14; NmbCrd = 3; break;
       case GmfPyramidsP3 :       NmbNod = 30; NmbCrd = 3; break;
       case GmfPyramidsP4 :       NmbNod = 55; NmbCrd = 3; break;
+      case GmfPrisms   :         NmbNod =  6; NmbCrd = 4; break;
       case GmfPrismsP2 :         NmbNod = 18; NmbCrd = 4; break;
       case GmfPrismsP3 :         NmbNod = 40; NmbCrd = 4; break;
       case GmfPrismsP4 :         NmbNod = 75; NmbCrd = 4; break;
+      case GmfHexahedra   :      NmbNod =  8; NmbCrd = 3; break;
       case GmfHexahedraQ2 :      NmbNod = 27; NmbCrd = 3; break;
       case GmfHexahedraQ3 :      NmbNod = 64; NmbCrd = 3; break;
       case GmfHexahedraQ4 :      NmbNod =125; NmbCrd = 3; break;
