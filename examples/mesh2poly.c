0d19cd0f3625f4b98cca9affe0e1d9d0feaba8c9
Loïc Maréchal <loic.marechal@inria.fr>
2021-05-12 16:13:46 +0200

    Added an example that convertes a surface mesh into a polygonal one.

---
+++ examples/mesh2poly.c
@@ -0,0 +1,146 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <libmeshb7.h>
+
+int main(int ArgCnt, char **ArgVec)
+{
+   int      i, ver, dim, ref, nod[8], ArgIdx = 1;
+   int      NmbVer, NmbTri, NmbQad, NmbTet, NmbPyr, NmbPri, NmbHex;
+   int64_t  InpMsh, OutMsh;
+   float    xf, yf, zf;
+   double   xd, yd, zd;
+   char    *InpNam, *OutNam;
+
+   if(ArgCnt != 3)
+   {
+      puts("\nMESH2POLY 1.0, may 12 2021, Loic MARECHAL / INRIA\n");
+      puts(" Usage    : mesh2poly source_name destination_name\n");
+      exit(0);
+   }
+
+   InpNam = ArgVec[ ArgIdx++ ];
+   OutNam = ArgVec[ ArgIdx++ ];
+
+   if(!strlen(InpNam))
+   {
+      puts("Missing input name.");
+      exit(1);
+   }
+
+   if(!strlen(OutNam))
+   {
+      puts("Missing output name.");
+      exit(2);
+   }
+
+   if(!strcmp(InpNam, OutNam))
+   {
+      puts("The output mesh cannot overwrite the input mesh.");
+      exit(3);
+   }
+
+   if(!(InpMsh = GmfOpenMesh(InpNam, GmfRead, &ver, &dim)))
+   {
+      printf("Cannot open mesh file %s.\n", InpNam);
+      return(4);
+   }
+
+   if(!(OutMsh = GmfOpenMesh(OutNam, GmfWrite, ver, dim)))
+   {
+      printf("Cannot open mesh file %s.\n", OutNam);
+      return(5);
+   }
+
+   NmbVer = GmfStatKwd(InpMsh, GmfVertices);
+   NmbTri = GmfStatKwd(InpMsh, GmfTriangles);
+   NmbQad = GmfStatKwd(InpMsh, GmfQuadrilaterals);
+   NmbTet = GmfStatKwd(InpMsh, GmfTetrahedra);
+   NmbPri = GmfStatKwd(InpMsh, GmfPrisms);
+   NmbPyr = GmfStatKwd(InpMsh, GmfPyramids);
+   NmbHex = GmfStatKwd(InpMsh, GmfHexahedra);
+
+   printf(" InpMsh: idx = %lld, version: %d, dimension: %d\n", InpMsh, ver, dim);
+
+   if(NmbVer)
+   {
+      printf(" Copying %d vertices\n", NmbVer);
+
+      GmfGotoKwd(InpMsh, GmfVertices);
+      GmfSetKwd(OutMsh, GmfVertices, NmbVer);
+
+      for(i=1;i<=NmbVer;i++)
+      {
+         if(ver <= 1)
+         {
+            GmfGetLin(InpMsh, GmfVertices, &xf, &yf, &zf, &ref);
+            GmfSetLin(OutMsh, GmfVertices,  xf,  xf,  zf,  ref);
+         }
+         else
+         {
+            GmfGetLin(InpMsh, GmfVertices, &xd, &yd, &zd, &ref);
+            GmfSetLin(OutMsh, GmfVertices,  xd,  xd,  zd,  ref);
+         }
+      }
+   }
+
+   if(NmbTri || NmbQad)
+   {
+      GmfSetKwd(OutMsh, GmfBoundaryPolygonHeaders, NmbTri + NmbQad);
+
+      if(NmbTri)
+      {
+         printf(" Adding  %d triangles to the polygons headers\n", NmbTri);
+         GmfGotoKwd(InpMsh, GmfTriangles);
+
+         for(i=1;i<=NmbTri;i++)
+         {
+            GmfGetLin(InpMsh, GmfTriangles, &nod[0], &nod[1], &nod[2], &ref);
+            GmfSetLin(OutMsh, GmfBoundaryPolygonHeaders, (i-1) * 3 + 1, ref);
+         }
+      }
+
+      if(NmbQad)
+      {
+         printf(" Adding  %d quads to the polygons headers\n", NmbQad);
+         GmfGotoKwd(InpMsh, GmfQuadrilaterals);
+
+         for(i=1;i<=NmbQad;i++)
+         {
+            GmfGetLin(InpMsh, GmfQuadrilaterals, &nod[0], &nod[1], &nod[2], &nod[3], &ref);
+            GmfSetLin(OutMsh, GmfBoundaryPolygonHeaders, (i-1) * 4 + NmbTri * 3, ref);
+         }
+      }
+
+      GmfSetKwd(OutMsh, GmfBoundaryPolygonVertices, NmbTri * 3 + NmbQad * 4);
+
+      if(NmbTri)
+      {
+         printf(" Adding  %d triangles to the polygons faces\n", NmbTri);
+         GmfGotoKwd(InpMsh, GmfTriangles);
+
+         for(i=1;i<=NmbTri;i++)
+         {
+            GmfGetLin(InpMsh, GmfTriangles, &nod[0], &nod[1], &nod[2], &ref);
+            GmfSetLin(OutMsh, GmfBoundaryPolygonVertices, nod[0], nod[1], nod[2]);
+         }
+      }
+
+      if(NmbQad)
+      {
+         printf(" Adding  %d quads to the polygons faces\n", NmbQad);
+         GmfGotoKwd(InpMsh, GmfQuadrilaterals);
+
+         for(i=1;i<=NmbQad;i++)
+         {
+            GmfGetLin(InpMsh, GmfQuadrilaterals, &nod[0], &nod[1], &nod[2], &nod[3], &ref);
+            GmfSetLin(OutMsh, GmfBoundaryPolygonVertices, nod[0], nod[1], nod[2], nod[3]);
+         }
+      }
+   }
+
+   GmfCloseMesh(InpMsh);
+   GmfCloseMesh(OutMsh);
+
+   return(0);
+}
--- todolist.md
+++ todolist.md
@@ -5,23 +5,24 @@
 - Give along a routine to convert to and from well-known high-order numberings.
 
 ### Distributed parallel write
-- Open a mesh file in write mode but only create the skeleton of the mesh structure to eneable further concurent write access.
-- Open an existing mesh file in write mode and enable concurent block write thanks to the existing structure.
+- Open a mesh file in writing mode but only create the skeleton of the mesh structure to enable further concurrent write access.
+- Open an existing mesh file in writing mode and enable concurrent block write thanks to the existing structure.
 
 ## STANDARD PRIORITY
 
 ### Handle arbitrary degree polygons and polyhedra
 - Add a helper that tesselate a polygon int a list of triangles made of vertex nodes and edge visibility flags.
-- Add a helper that cuts a polyhedron through a plane and generates the intersetion's triangulated mesh in a STL-like format.
+- Add a helper that cuts a polyhedron through a plane and generates the intersection's triangulated mesh in an STL-like format.
+- Add an option to transmesh that converts and fuse all surface and volume elements to polygons and polyhedra.
 
 
 ### Solution fields comments
 - Add a procedure that would search for a string among comments.
 - Input:  keyword name, physical property, free comment, wildcards.
-- Output: list of solutions keywords and particuliar field number.
+- Output: list of solutions keywords and particular field number.
 
 ### Arbitrary reference element
-- Add a set of keywords for each kind of elements that stores the number of vertices and their barycentric coordinates.
+- Add a set of keywords for each kind of element that stores the number of vertices and their barycentric coordinates.
 
 ### Convert HO examples to Fortran
 - test_libmeshb_HO.c
@@ -30,8 +31,8 @@
 ### Add F77 API to GmfSetHONodesOrdering
 An easy one.
 
-### Add IHOSol* + DHOSol* for each element kinds
-for example :
+### Add IHOSol* + DHOSol* for each element kinds,
+for example
 "IHOSolAtVertices",                           "i", "ii" // ii = degree + index in DSol
 "DHOSolAtVertices",                           "i", "hr" // High Order solution
 
@@ -43,10 +44,10 @@ for example :
 - Added ByteFlow keyword to store an arbitrary sized byte array
 - Added two examples to read and write EGADS CAD models stores as byte flows
 - Give a way to describe the node numbering.
-- Setup a polygon keyword thats provides an arbitrary degree and number of nodes.
-- Setup a polyhedra keyword thats provides an arbitrary degree and number of oriented polygons.
-- Setup a boundary polygons keyword that lists the boundary faces among the enemble of volume polygons.
-- Create a separate helpers file to be optionaly compiled along the libMeshb.
+- Setup a polygon keyword that provides an arbitrary degree and number of nodes.
+- Setup a polyhedron keyword that provides an arbitrary degree and number of oriented polygons.
+- Setup a boundary polygon keyword that lists the boundary faces among the ensemble of volume polygons.
+- Create a separate helper file to be optionally compiled along the libMeshb.
 - Add a helper to allocate and read the boundary polygons.
 - Add a helper to allocate and read the polyhedra and inner polygons.
 - Add a helper that returns a polygon's degree and nodes list.
