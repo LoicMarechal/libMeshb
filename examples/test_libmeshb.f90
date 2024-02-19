0b42c627a7f3d48ccdba3d71c1d57007e62e4165
Christophe Peyret <christophe.peyret@gmail.com>
2024-02-19 11:52:12 +0100

    Ajout version fortran 90

---
+++ examples/test_libmeshb.f90
@@ -0,0 +1,127 @@
+
+! libMeshb 7.79 basic example:
+! read a quad mesh, split it into triangles and write the result back
+! write an associated dummy .sol file containing some data
+
+program  test_libmeshb_f90
+  !>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+  use iso_fortran_env
+  use libmeshb7
+  !<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+  !>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+  implicit none
+  
+  integer               :: i
+  integer               :: NmbVer,NmbQad,ver,dim,res,kwd
+  integer               :: t(1:10),d,ho,s
+  integer(8)            :: InpMsh, OutMsh
+  real(real64)          :: sol(1:10)
+  real(real64), pointer :: VerTab(:,:)
+  integer     , pointer :: VerRef(  :)
+  integer     , pointer :: QadTab(:,:)
+  integer     , pointer :: QadRef(  :)
+  !<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+  
+  !>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+  ! Open the quadrilateral mesh file for reading
+  
+  ! Open the mesh file and check the version and dimension
+  InpMsh = GmfOpenMeshf77('../sample_meshes/quad.mesh ', GmfRead,ver,dim)
+  print '("Input mesh :",i0," version: ",i0," dim: ",i0)',InpMsh,ver,dim
+  if( InpMsh==0) stop ' InpMsh = 0'
+  if( ver<=1   ) stop ' version <= 1'
+  if( dim/=3   ) stop ' dimension <> 3'
+  
+  ! Allocate VerTab and VerRef
+  NmbVer = Gmfstatkwdf77(InpMsh, GmfVertices, 0, s, t, d, ho)
+  allocate(VerTab(1:3,1:NmbVer))
+  allocate(VerRef(    1:NmbVer))
+  
+  ! Allocate QadTab and QadRef
+  NmbQad=Gmfstatkwdf77(InpMsh, GmfQuadrilaterals, 0, s, t, d, ho)  
+  allocate(QadTab(1:4,1:NmbQad))
+  allocate(QadRef(    1:NmbQad))
+  
+  print '("Input mesh : ",i0," vertices: ",i0," quads")',NmbVer,NmbQad
+  
+  ! Read the quads
+  res=Gmfgotokwdf77(InpMsh, GmfQuadrilaterals)
+  do i=1,NmbQad
+    res=GmfGetElement(InpMsh, GmfQuadrilaterals, QadTab(1:4,i), QadRef(i))
+  enddo
+  
+  ! Read the vertices
+  res = Gmfgotokwdf77(InpMsh, GmfVertices)
+  do i=1,NmbVer
+    res=GmfGetVertex(InpMsh, VerTab(1:3,i), VerRef(i))
+  end do
+  
+  ! Close the quadrilateral mesh
+  res=GmfCloseMeshf77(InpMsh)
+  !<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+  !>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+  ! Create a triangular mesh
+  
+  OutMsh=GmfOpenMeshf77('tri.mesh', GmfWrite, 2, 3)
+  if(OutMsh==0) STOP ' OutMsh = 0'
+  print '(/"output tri.mesh  IDX: ",i0)',OutMsh
+  
+  ! Set the number of vertices
+  res=GmfSetKwdf77(OutMsh, GmfVertices, NmbVer, 0, t, 0, ho)
+  
+  ! Then write them down
+  do i=1,NmbVer
+    res=GmfSetVertex(OutMsh, VerTab(1:3,i), VerRef(i))
+  end do
+  
+  ! Write the triangles
+  res = Gmfsetkwdf77(OutMsh, GmfTriangles, 2*NmbQad, 0, t, 0, ho)
+  do i=1,NmbQad
+    res=GmfSetElement(OutMsh, GmfTriangles, QadTab(1,i), QadRef(i))
+    !     Modify the quad to build the other triangle's diagonal
+    QadTab(2,i) = QadTab(3,i);
+    QadTab(3,i) = QadTab(4,i);
+    res=GmfSetElement(OutMsh, GmfTriangles, QadTab(1,i), QadRef(i))
+  end do
+  
+  ! Don't forget to close the file
+  res = GmfCloseMeshf77(OutMsh)
+  
+  
+  print '("output mesh: ",i0," vertices: ",i0," triangles")',NmbVer,2*NmbQad
+  !<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+  
+  !>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+  !> Cleanning Memory
+  deallocate(VerTab,VerRef)
+  deallocate(QadTab,QadRef)
+  !<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+  
+  !>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+  ! Create a solution file
+  
+  OutMsh = GmfOpenMeshf77('tri.sol', GmfWrite, 2, 3)
+  if(OutMsh==0) STOP ' OutMsh = 0'
+  print '("output IDX: ",i0)',OutMsh
+  
+  ! Set the solution kinds
+  t(1:3) = [GmfSca,GmfVec,GmfSca]
+  
+  ! Set the number of solutions (one per vertex)
+  res = Gmfsetkwdf77(OutMsh, GmfSolAtVertices, NmbVer, 3, t, 0, ho)
+  
+  ! Write the dummy solution fields
+  do i = 1, NmbVer
+    sol(1:4) = [i,2*i,3*i,4*i]
+    sol(5) = -i
+    res = Gmfsetsolution(OutMsh, GmfSolAtVertices, sol)
+  end do
+
+  ! Don't forget to close the file
+  res = GmfCloseMeshf77(OutMsh)
+  
+  print '("output sol: ",i0," solutions")',NmbVer
+  !<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+end program test_libmeshb_f90
---
+++ examples/test_libmeshb_block.f90
@@ -0,0 +1,133 @@
+
+! libMeshb 7.79 example: transform a quadrilateral mesh into a triangular one
+! using fast block transfer
+
+program test_libmeshb_block_f90
+  !>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+  use iso_fortran_env
+  use libmeshb7
+  !<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+  !>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+  implicit none
+  integer               :: i
+  integer               :: NmbVer,NmbQad,NmbTri,ver,dim,res
+  integer(8)            :: InpMsh, OutMsh, m(1)
+  real(real64)          :: sol(1:10)
+  real(real64), pointer :: VerTab(:,:)
+  integer     , pointer :: VerRef(  :)
+  integer     , pointer :: QadTab(:,:),QadRef(  :)
+  integer     , pointer :: TriTab(:,:),TriRef(  :)
+  integer               :: t(1),d,ho,s
+  !<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+  !>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+  ! Open the quadrilateral mesh file for reading
+  
+  ! Open the mesh file and check the version and dimension
+  InpMsh = GmfOpenMeshf77('../sample_meshes/quad.mesh ', GmfRead,ver,dim)
+  print '("Input mesh :",i0," version: ",i0," dim: ",i0)',InpMsh,ver,dim
+  if( InpMsh==0) stop ' InpMsh = 0'
+  if( ver<=1   ) stop ' version <= 1'
+  if( dim/=3   ) stop ' dimension <> 3'
+  
+  ! Allocate VerTab and VerRef
+  NmbVer = Gmfstatkwdf77(InpMsh, GmfVertices, 0, s, t, d, ho)
+  allocate(VerTab(1:3,1:NmbVer))
+  allocate(VerRef(    1:NmbVer))
+  
+  ! Allocate QadTab and QadRef
+  NmbQad=Gmfstatkwdf77(InpMsh, GmfQuadrilaterals, 0, s, t, d, ho)  
+  allocate(QadTab(1:4,1:NmbQad))
+  allocate(QadRef(    1:NmbQad))
+  
+  print '("Input mesh : ",i0," vertices: ",i0," quads")',NmbVer,NmbQad
+
+  print '("input mesh: ",i0)', InpMsh
+  print '("version   : ",i0)', ver
+  print '("dimension : ",i0)', dim
+  print '("vertices  : ",i0)', NmbVer
+  print '("quads     : ",i0)', NmbQad
+  
+  ! Read the vertices using a vector of 3 consecutive doubles to store the coordinates
+  res=GmfGetVertices(                &
+  &   InpMsh                        ,&
+  &   1, NmbVer, 0, m               ,&
+  &   VerTab(1,1), VerTab(1,NmbVer) ,&
+  &   VerRef(  1), VerRef(  NmbVer)  )
+  
+  ! Read the quads using one single vector of 5 consecutive integers
+  res=GmfGetElements(               &
+  &   InpMsh                       ,&
+  &   GmfQuadrilaterals            ,&
+  &   1, NmbQad, 0, m              ,&
+  &   QadTab(1,1), QadTab(1,NmbQad),&
+  &   QadRef(  1), QadRef(  NmbQad) )
+
+  ! Close the quadrilateral mesh
+  res=GmfCloseMeshf77(InpMsh)
+  !<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+  
+  !>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+  ! Convert the quad mesh into a triangular one
+  
+  NmbTri=2*NmbQad
+  allocate(TriTab(1:3,1:NmbTri))
+  allocate(TriRef(    1:NmbTri))
+  
+  do i=1,NmbTri
+    if(mod(i,2) .EQ. 1) then
+      TriTab(1,i) = QadTab(1,(i+1)/2)
+      TriTab(2,i) = QadTab(2,(i+1)/2)
+      TriTab(3,i) = QadTab(3,(i+1)/2)
+      TriRef(  i) = QadRef(  (i+1)/2)
+    else
+      TriTab(1,i) = QadTab(1,(i+1)/2)
+      TriTab(2,i) = QadTab(3,(i+1)/2)
+      TriTab(3,i) = QadTab(4,(i+1)/2)
+      TriRef(  i) = QadRef(  (i+1)/2)
+    endif
+  end do
+  !<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+  
+  !>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+  ! Write a triangular mesh
+  
+  OutMsh = GmfOpenMeshf77('tri.meshb', GmfWrite, ver, dim)
+  if(OutMsh==0) STOP ' OutMsh = 0'
+  
+  ! Set the number of vertices
+  res=Gmfsetkwdf77(OutMsh, GmfVertices, NmbVer, 0, t, 0, ho)
+  
+  ! Write them down using separate pointers for each scalar entry
+  res=Gmfsetvertices(               &
+  &   OutMsh                       ,&
+  &   1, NmbVer, 0, m              ,&
+  &   VerTab(1,1), VerTab(1,NmbVer),&
+  &   VerRef(  1), VerRef(  NmbVer) )
+  
+  ! Write the triangles using 4 independant set of arguments 
+  ! for each scalar entry: node1, node2, node3 and reference
+  res=Gmfsetkwdf77(OutMsh, GmfTriangles, NmbTri, 0, t, 0, ho)
+  res = GmfSetElements(               &
+  &     OutMsh                       ,&
+  &     GmfTriangles                 ,&
+  &     1, 2*NmbQad, 0, m            ,&
+  &     TriTab(1,1), TriTab(1,NmbTri),&
+  &     TriRef(  1), TriRef(  NmbTri) )
+  
+  ! Don't forget to close the file
+  res = GmfCloseMeshf77(OutMsh)
+  
+  print '("output mesh :",i0," vertices: ",i0," triangles")',NmbVer,NmbTri
+  !<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+  
+  !>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+  !> Cleanning Memory
+  deallocate(VerTab,VerRef)
+  deallocate(QadTab,QadRef)
+  deallocate(TriTab,TriRef)
+  !<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+  
+
+end program test_libmeshb_block_f90
+
